c27f1c42a774eae201063a8f128b3847b88676fc
Duc Thanh Nguyen <dnguyen@kolb-digital.de>
2025-05-27 15:56:06 +0200

    clean code

---
+++ frontend/src/app/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    allowedHosts: ['mybookstore-angular.onrender.com'],
+    host: true,
+    port: 4200,
+  },
+});
